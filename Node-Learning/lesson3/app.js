c254cfa9530794f31b1da96b067531ba30d4392c
Nero Zuo <502350123@qq.com>
2017-07-25 01:56:48 +0800

    node lesson3

---
+++ Node-Learning/lesson3/app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var superagent = require('superagent');
+var cheerio = require('cheerio');
+
+var app = express();
+
+app.get('/', function(req, res, next) {
+    superagent.get("http://cnodejs.org")
+        .end(function(err, reponse) {
+            if (err) {
+                return next(err)
+            }
+            var $ = cheerio.load(res.text);
+            var items = [];
+            $('#topic_list .topic_title').each(function (idx, element) {
+                var $element = $(element);
+                items.push({
+                    title: $element.attr('title'),
+                    href: $element.attr('href')
+                });
+            });
+            res.send(items);
+        });
+})
+
+app.listen(8080, function(req, res) {
+    console.log("app is running at port 8080");
+});
